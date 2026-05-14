f0d8e4c0d85e1667e57fccdde8ffce12380a6e8c
Miguel Pereira <miguelarnaud5@gmail.com>
2026-05-14 15:21:58 +0100

    React config

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+  return (
+    <main className="app-shell">
+      <h1>ReactJS App Created</h1>
+      <p>
+        Edit <code>src/App.jsx</code> to start building your interface.
+      </p>
+    </main>
+  )
+}
+
+export default App
